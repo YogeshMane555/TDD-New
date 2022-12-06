260b02741a250ff41a1200f4fb5868bae488dc04
Yogesh <Yogesh@DESKTOP-NLPPFJ1>
2022-12-06 20:17:24 +0530

    6-12 Second Commit

--- src/test/java/practice/practice6_12.java
+++ src/test/java/practice/practice6_12.java
@@ -2,6 +2,7 @@ package practice;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
@@ -11,6 +12,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -63,10 +65,87 @@ public class practice6_12 {
 	}
 	
 	@Test
-	public void tc2()
+	public void tc2() throws InterruptedException
 	{
+		rm.findElement(By.xpath("//*[contains(@id,'autocomplete')]")).sendKeys("bi");
 		
+		Thread.sleep(4000);
+		
+		List<WebElement> auto=rm.findElements(By.xpath("//*[contains(@class,'ui-menu-item')]"));
+		
+		int sizeOfAuto=auto.size();
+		
+		System.out.println("Total Number of List is :" +sizeOfAuto);
+		
+		for(WebElement sug:auto)
+		{
+			String SuggestionList=sug.getText();
+			
+			System.out.println("AutoSuggestion Text are :" +SuggestionList);
+			
+		}
+		
+		TakesScreenshot tk=(TakesScreenshot)rm;
+		
+		File src=tk.getScreenshotAs(OutputType.FILE);
+		
+		try {
+			FileUtils.copyFile(src, new File("C://Users//Yogesh//eclipse-workspace//TDD//Screenshot//AutoSuggestionList.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
+	@Test
+	public void tc3() throws InterruptedException
+	{
+		WebElement drpdwn= rm.findElement(By.xpath("//*[contains(@id,'dropdown-class-example')]"));
+		
+		Select s=new Select(drpdwn);
+		
+		s.selectByVisibleText("Option2");
+		
+		Thread.sleep(4000);
+		
+		TakesScreenshot tk=(TakesScreenshot)rm;
+		
+		File src=tk.getScreenshotAs(OutputType.FILE);
+		
+		try {
+			FileUtils.copyFile(src, new File("C://Users//Yogesh//eclipse-workspace//TDD//Screenshot//DropDownScreenshot.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	@Test
+	public void tc4() throws InterruptedException
+	{
+		WebElement check= rm.findElement(By.xpath("//*[contains(@id,'checkBoxOption2')]"));
+		
+		check.click();
+		
+		boolean b2=check.isSelected();
+		
+		System.out.println("Is Checkbox value 2 Selected ? :"+ b2);
+		
+		Thread.sleep(4000);
+		
+		TakesScreenshot tk=(TakesScreenshot)rm;
+	
+		File src=tk.getScreenshotAs(OutputType.FILE);
+	
+		try {
+			FileUtils.copyFile(src, new File("C://Users//Yogesh//eclipse-workspace//TDD//Screenshot//CheckBoxScreenshot.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	
 	
 	@AfterMethod
